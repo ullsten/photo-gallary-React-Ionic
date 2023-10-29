963823a97a863ef2bbb8c0164d8c9435372c272d
ullzten <ullzten@gmail.com>
2023-10-29 22:53:27 +0100

    added delete photo

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,4 @@
-import { camera, trash, close } from 'ionicons/icons';
+import { camera, trash, close, text } from 'ionicons/icons';
 import { 
   IonContent, 
   IonHeader, 
@@ -16,10 +16,12 @@ import {
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
-import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
+import React,{useState} from 'react';
 
 const Tab2: React.FC = () => {
-  const {photos, takePhoto} = usePhotoGallery();
+  const {photos, takePhoto, deletePhoto} = usePhotoGallery();
+  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
 
   return (
     <IonPage>
@@ -40,7 +42,7 @@ const Tab2: React.FC = () => {
         <IonRow>
           {photos.map((photo, index) => (
             <IonCol size='6' key={photo.filepath}>
-              <IonImg src={photo.webviewPath}></IonImg>
+              <IonImg onClick={() => setPhotoToDelete(photo)} src={photo.webviewPath}></IonImg>
             </IonCol>
           ))}
         </IonRow>
@@ -50,6 +52,28 @@ const Tab2: React.FC = () => {
           <IonIcon icon={camera}></IonIcon>
         </IonFabButton>
       </IonFab>
+      <IonActionSheet
+      isOpen={!!photoToDelete}
+      buttons={[
+        {
+          text: "Delete",
+          role: "destructive",
+          icon: trash,
+          handler: () => {
+            if (photoToDelete) {
+              deletePhoto(photoToDelete);
+              setPhotoToDelete(undefined);
+            }
+          },
+        },
+        {
+          text: "Cancel",
+          icon: close,
+          role: "cancel",
+        },
+      ]}
+      onDidDismiss={() => setPhotoToDelete(undefined)}
+      ></IonActionSheet>
       </IonContent>
     </IonPage>
   );
