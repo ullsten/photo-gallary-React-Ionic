ceb5ff3bbe9fa024111f27d95c257fe89836cbbc
ullzten <ullzten@gmail.com>
2023-10-29 23:39:28 +0100

    added appflow sdk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,19 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallary',
   webDir: 'dist',
+  plugins: {
+    LiveUpdates: {
+      appId: 'c71adc50',
+      channel: 'production',
+      autoUpdateMethod: 'none',
+      maxVersions: 2,
+    }
+  },
+  
   server: {
     androidScheme: 'https'
-  }
+  },
+  
 };
 
 export default config;
